d1ea48f4a9dd3f194640488ba3bbb95576a53427
Craig Newton <newtondev@gmail.com>
2023-11-30 21:48:36 +0100

    Added 2022 day 1 solution.

---
+++ 2022/README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2023 🎄
+
+My solutions to the [Advent of Code 2022](https://adventofcode.com/2022) in Rust
+
---
+++ 2022/src/bin/01.rs
@@ -0,0 +1,37 @@
+use aoc::read_file_input;
+
+fn main() {
+    let input = read_file_input("01.txt".to_string());
+
+    let mut current = 0;
+    let mut max: (i32, i32, i32) = (0, 0, 0);
+
+    for line in input.lines() {
+        if line.is_empty() {
+            max = calculate(current, max);
+            current = 0;
+        } else {
+            current += line.trim().parse::<i32>().unwrap()
+        }
+    }
+
+    max = calculate(current, max);
+
+    println!("{}", max.0);
+    println!("{}", max.0 + max.1 + max.2)
+}
+
+fn calculate(current: i32, max: (i32, i32, i32)) -> (i32, i32, i32) {
+    let mut max = max;
+    if current > max.0 {
+        max.2 = max.1;
+        max.1 = max.0;
+        max.0 = current;
+    } else if current > max.1 {
+        max.2 = max.1;
+        max.1 = current;
+    } else if current > max.2 {
+        max.2 = current;
+    }
+    return max;
+}
