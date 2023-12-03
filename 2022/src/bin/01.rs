4405df453ad316cd1da34a0d608438bb09aad03d
Craig Newton <newtondev@gmail.com>
2023-12-03 17:11:57 +0100

    Updated 2022/01.

--- 2022/src/bin/01.rs
+++ 2022/src/bin/01.rs
@@ -1,8 +1,14 @@
+// See: https://adventofcode.com/2022/day/1
 use aoc::read_file_input;
 
 fn main() {
-    let input = read_file_input("01.txt".to_string());
+    let res = solve(read_file_input("01.txt".to_string()));
 
+    println!("{}", res.0);
+    println!("{}", res.1);
+}
+
+fn solve(input: String) -> (i32, i32) {
     let mut current = 0;
     let mut max: (i32, i32, i32) = (0, 0, 0);
 
@@ -17,8 +23,7 @@ fn main() {
 
     max = calculate(current, max);
 
-    println!("{}", max.0);
-    println!("{}", max.0 + max.1 + max.2)
+    (max.0, max.0 + max.1 + max.2)
 }
 
 fn calculate(current: i32, max: (i32, i32, i32)) -> (i32, i32, i32) {
@@ -34,4 +39,28 @@ fn calculate(current: i32, max: (i32, i32, i32)) -> (i32, i32, i32) {
         max.2 = current;
     }
     return max;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use aoc::read_test_file_input;
+
+    #[test]
+    fn test_solve_one() {
+        let res = solve(read_test_file_input("01_one.txt".to_string()));
+        assert_eq!(
+            res.0,
+            24000
+        );
+    }
+
+    #[test]
+    fn test_solve_two() {
+        let res = solve(read_test_file_input("01_one.txt".to_string()));
+        assert_eq!(
+            res.1,
+            45000
+        );
+    }
 }
